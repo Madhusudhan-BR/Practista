cbccac9509622e06b4529c0f721ab9c57e95f179
Madhusudhan-BR <madhusudhanbram@gmail.com>
2018-05-28 11:20:30 -0400

    Initial Commit

---
+++ Practista/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Practista
+//
+//  Created by Madhusudhan B.R on 5/28/18.
+//  Copyright © 2018 Madhusudhan B.R. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
